306342baac88ca73426a4f8cbe8d4b0b40398a23
FiniteLooper <chris.m.barr@gmail.com>
2023-09-24 19:09:24 -0400

    A few changes needed for a prod environment

--- task-management/src/app.module.ts
+++ task-management/src/app.module.ts
@@ -17,17 +17,25 @@ import { configValidationSchema } from './config.schema';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        autoLoadEntities: true,
-        synchronize: true,
-        entities: [TaskEntity, UserEntity],
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        database: configService.get<string>('DB_DATABASE'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-      }),
+      useFactory: async (configService: ConfigService) => {
+        const isProduction = configService.get('ENVIRONMENT') === 'production';
+
+        return {
+          ssl: isProduction,
+          extra: {
+            ssl: isProduction ? { rejectUnauthorized: false } : null,
+          },
+          type: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+          entities: [TaskEntity, UserEntity],
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          database: configService.get('DB_DATABASE'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+        };
+      },
     }),
     AuthModule,
     TasksModule,
